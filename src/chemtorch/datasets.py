820bfb4549df83fb513b118b2e69d959488780da
jacob <jwgerlach00@gmail.com>
2022-06-26 11:51:02 -0400

    smiles dataset

---
+++ src/chemtorch/datasets.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+
+class SmilesDataset(Dataset):
+    def __init__(self, smiles:Iterable, targets:Iterable, vocab_table:str, pad_len:int):
+        self.smiles = smiles
+        self.targets = targets
+        self.vocab_table = vocab_table
+        self.pad_len = pad_len
+
+    def __len__(self):
+        return len(self.targets)
+
+    def __getitem__(self, idx):
+        input = self.smiles[idx]
+        target = self.targets[idx]
+
+        return input, target, self.vocab_table, self.pad_len
